c8d1bfbfbd1517f911d7665c877639d2197e9ec8
zhelonkin <Zhelonkin.ru@gmail.com>
2019-04-10 00:53:14 +0400

    Еще чутка доделок

--- Desktop/Windows/MainWindow.xaml.cs
+++ Desktop/Windows/MainWindow.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
 using System.Windows;
@@ -10,23 +10,68 @@ namespace Desktop.Windows
     public partial class MainWindow
     {
         private readonly AstronomerDbContext _context;
-        private IList<HeavenlyBody> _bodies;
+        private ObservableCollection<HeavenlyBody> _bodies;
 
         public MainWindow()
         {
             InitializeComponent();
 
             _context = new AstronomerDbContext();
-            _bodies = new List<HeavenlyBody>();
+            _bodies = new ObservableCollection<HeavenlyBody>();
+
+            _bodiesDataGrid.ItemsSource = _bodies;
         }
 
         private void SearchButton_OnClick(object sender, RoutedEventArgs e)
         {
             var searchQuery = _searchTextBox.Text;
 
-            _bodiesDataGrid.ItemsSource = _context.HeavenlyBodies
-                .Where(body => body.Name == searchQuery)
-                .ToList();
+            _bodies.Clear();
+            switch (_fieldComboBox.SelectedIndex)
+            {
+                case 0:
+                    _context.HeavenlyBodies
+                        .Where(body => body.Name.Contains(searchQuery))
+                        .ToList()
+                        .ForEach(_bodies.Add);
+                    break;
+                case 1:
+                    _context.HeavenlyBodies
+                        .Where(body => body.Type.Contains(searchQuery))
+                        .ToList()
+                        .ForEach(_bodies.Add);
+                    break;
+                case 2:
+                    if (double.TryParse(searchQuery, out var distance))
+                    {
+                        _context.HeavenlyBodies
+                            .Where(body => body.Distance == distance)
+                            .ToList()
+                            .ForEach(_bodies.Add);
+                    }
+
+                    break;
+                case 3:
+                    if (double.TryParse(searchQuery, out var mass))
+                    {
+                        _context.HeavenlyBodies
+                            .Where(body => body.Mass == mass)
+                            .ToList()
+                            .ForEach(_bodies.Add);
+                    }
+
+                    break;
+                case 4:
+                    if (double.TryParse(searchQuery, out var radius))
+                    {
+                        _context.HeavenlyBodies
+                            .Where(body => body.Radius == radius)
+                            .ToList()
+                            .ForEach(_bodies.Add);
+                    }
+
+                    break;
+            }
         }
 
         private void AddButton_OnClick(object sender, RoutedEventArgs e)
@@ -38,11 +83,24 @@ namespace Desktop.Windows
                 return;
             }
 
-            _bodiesDataGrid.Items.Add(body);
+            _bodies.Add(body);
             _context.HeavenlyBodies.Add(body);
             _context.SaveChanges();
         }
 
+        private void RemoveButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            var selectedRow = _bodiesDataGrid.SelectedIndex;
+            if (selectedRow < 0 || _bodies.Count <= selectedRow)
+            {
+                return;
+            }
+
+            var body = _bodies[selectedRow];
+            _bodies.RemoveAt(selectedRow);
+            _context.HeavenlyBodies.Remove(body);
+            _context.SaveChanges();
+        }
 
         private void MainWindow_Closing(object sender, CancelEventArgs e)
         {
